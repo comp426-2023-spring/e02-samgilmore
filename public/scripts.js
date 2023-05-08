35690130d3f6d10226997ec93a648e0fe136ec14
samgilmore <30483214+samgilmore@users.noreply.github.com>
2023-05-08 13:30:54 -0700

    Final w/ formatting

--- public/scripts.js
+++ public/scripts.js
@@ -1,82 +1,97 @@
-// If you would like to see some examples of similar code to make an interface interact with an API, 
+// If you would like to see some examples of similar code to make an interface interact with an API,
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
 
-const gameModeSelect = document.getElementById('game-mode');
-const playModeInputs = document.getElementsByName('play-mode');
-const choiceInputs = document.getElementsByName('choice');
-const playButton = document.getElementById('play-button');
-const resetButton = document.getElementById('reset-button');
-const resultsSection = document.getElementById('results-section');
-const resultText = document.getElementById('result-text');
+const gameModeSelect = document.getElementById("game-mode");
+const playModeInputs = document.getElementsByName("play-mode");
+const choiceInputs = document.getElementsByName("choice");
+const playButton = document.getElementById("play-button");
+const resetButton = document.getElementById("reset-button");
+const resultsSection = document.getElementById("results-section");
+const resultText = document.getElementById("result-text");
 
 let gameMode = gameModeSelect.value;
 let playMode = playModeInputs[0].value;
 
-gameModeSelect.addEventListener('change', () => {
+gameModeSelect.addEventListener("change", () => {
   gameMode = gameModeSelect.value;
   updateChoices();
 });
 
 for (let input of playModeInputs) {
-  input.addEventListener('change', () => {
+  input.addEventListener("change", () => {
+    if (input.value === "random") {
+      document.getElementById("choices-section").style.display = "none";
+    } else {
+      document.getElementById("choices-section").style.display = "inline-block";
+    }
+
     playMode = input.value;
   });
 }
 
 playButton.addEventListener("click", async () => {
-    const choice = playMode === "opponent" ? getSelectedChoice() : undefined;
-  
-    let url = `http://localhost:8080/app/${gameMode}/`;
-    let fetchOptions = {};
-  
-    if (choice !== undefined) {
-      if (gameMode === "rps") {
-        url += "/play/";
-      } else if (gameMode === "rpsls") {
-        url += "/play/";
-      }
-  
-      fetchOptions = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ shot: choice }),
-      };
-    }
-  
-    try {
-      const response = await fetch(url, fetchOptions);
-      const result = await response.json();
-  
-      if (result === "error") {
-        alert("An error occurred. Please try again.");
-      } else {
-        displayResult(result);
-      }
-    } catch (error) {
-      alert("An error occurred. Please try again.");
-    }
-  });
+  const choice = getSelectedChoice();
+  const playMode = getSelectedPlayMode();
 
-resetButton.addEventListener('click', () => {
-  resultText.textContent = '';
-  resultsSection.style.display = 'none';
+  let url = `http://localhost:8080/app/${gameMode}/`;
+
+  console.log(choice, playMode, url);
+
+  if (playMode === "opponent") {
+    url += `play/${choice}`;
+  }
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data);
+
+    displayResult(data);
+  } catch (error) {
+    alert("An error occurred. Please try again.");
+  }
+
+  //hide everything & show reset
+  document.getElementById("game-mode-section").style.display = "none";
+  document.getElementById("play-mode-section").style.display = "none";
+  document.getElementById("choices-section").style.display = "none";
+  document.getElementById("play-button").style.display = "none";
+  document.getElementById("reset-button").style.display = "inline-block";
+});
+
+resetButton.addEventListener("click", () => {
+  resultText.textContent = "";
+  resultsSection.style.display = "none";
+
+  //show everything & hide reset
+  document.getElementById("game-mode-section").style.display = "block";
+  document.getElementById("play-mode-section").style.display = "block";
+
+  if (playMode === "opponent") {
+    document.getElementById("choices-section").style.display = "block";
+  }
+
+  document.getElementById("play-button").style.display = "inline-block";
+  document.getElementById("reset-button").style.display = "none";
 });
 
 function updateChoices() {
-  if (gameMode === 'rps') {
-    document.getElementById('lizard').style.display = 'none';
-    document.querySelector('label[for="lizard"]').style.display = 'none';
-    document.getElementById('spock').style.display = 'none';
-    document.querySelector('label[for="spock"]').style.display = 'none';
+  if (gameMode === "rps") {
+    document.getElementById("lizard").style.display = "none";
+    document.querySelector('label[for="lizard"]').style.display = "none";
+    document.getElementById("spock").style.display = "none";
+    document.querySelector('label[for="spock"]').style.display = "none";
   } else {
-    document.getElementById('lizard').style.display = 'inline-block';
-    document.querySelector('label[for="lizard"]').style.display = 'inline-block';
-    document.getElementById('spock').style.display = 'inline-block';
-    document.querySelector('label[for="spock"]').style.display = 'inline-block';
+    document.getElementById("lizard").style.display = "inline-block";
+    document.querySelector('label[for="lizard"]').style.display =
+      "inline-block";
+    document.getElementById("spock").style.display = "inline-block";
+    document.querySelector('label[for="spock"]').style.display = "inline-block";
   }
+
+  document.getElementById("rock").checked = true;
 }
 
 function getSelectedChoice() {
@@ -87,9 +102,22 @@ function getSelectedChoice() {
   }
 }
 
+function getSelectedPlayMode() {
+  for (let input of playModeInputs) {
+    if (input.checked) {
+      return input.value;
+    }
+  }
+}
+
 function displayResult(result) {
-  resultsSection.style.display = 'block';
-  resultText.textContent = `You played: ${result.player.toUpperCase()}. Opponent played: ${result.opponent.toUpperCase()}. Result: ${result.result.toUpperCase()}.`;
+  resultsSection.style.display = "block";
+
+  if (playMode === "opponent") {
+    resultText.textContent = `You played: ${result.player.toUpperCase()}. Opponent played: ${result.opponent.toUpperCase()}. Result: ${result.result.toUpperCase()}.`;
+  } else {
+    resultText.textContent = `Random draw: ${result.player.toUpperCase()}.`;
+  }
 }
 
 updateChoices();
--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 //// Load most basic dependencies
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-// Create require function 
+// Create require function
 // https://nodejs.org/docs/latest-v18.x/api/module.html#modulecreaterequirefilename
-import { createRequire } from 'node:module';
+import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
 // The above two lines allow us to use ES methods and CJS methods for loading
 // dependencies.
 // Load minimist for command line argument parsing
 // https://www.npmjs.com/package/minimist
-const minimist = require('minimist')
+const minimist = require("minimist");
 // Parse our command line arguments
-const args = minimist(process.argv.slice(2))
+const args = minimist(process.argv.slice(2));
 // Are we debugging or testing?
 // If so, then let's look at our command line arguments just to see what is in there
 if (args.debug) {
-    console.info('Minimist parsed and created the following `args` object:')
-    console.info(args)
+  console.info("Minimist parsed and created the following `args` object:");
+  console.info(args);
 }
-// Did we call for help? 
+// Did we call for help?
 if (args.h || args.help) {
-    console.log(`
+  console.log(`
 usage: node server.js --port=5000
 
 This package serves the static HTML, CSS, and JS files in a /public directory.
@@ -38,137 +38,155 @@ It also creates logs in a common log format (CLF) so that you can better.
                     (Does not work when run with nodemon)
   --debug       Echos more information to STDOUT so that you can see what is
                     stored in internal variables, etc.
-    `)
-    process.exit(0)
-} 
+    `);
+  process.exit(0);
+}
 // Load express and other dependencies for serving HTML, CSS, and JS files
-import express from 'express'
+import express from "express";
 // Use CJS __filename and __dirname in ES module scope
 // https://flaviocopes.com/fix-dirname-not-defined-es-module-scope/
-import path from 'path'
-import { fileURLToPath } from 'url'
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // Load dependencies for logging
-const fs = require('fs')
-const morgan = require('morgan')
+const fs = require("fs");
+const morgan = require("morgan");
 // Create log path
-const logpath = args.log || args.l || process.env.LOGPATH || path.join(__dirname, 'log')
-if (!fs.existsSync(logpath)){
-    fs.mkdirSync(logpath);
+const logpath =
+  args.log || args.l || process.env.LOGPATH || path.join(__dirname, "log");
+if (!fs.existsSync(logpath)) {
+  fs.mkdirSync(logpath);
 }
 if (args.debug) {
-    console.info('HTTP server is logging to this directory:')
-    console.info(logpath)
+  console.info("HTTP server is logging to this directory:");
+  console.info(logpath);
 }
 // Create an app server
-const app = express()
+const app = express();
+
+var cors = require("cors");
+app.use(cors());
+
 // Set a port for the server to listen on
-const port = process.env.PORT || args.port || args.p || 8080
+const port = process.env.PORT || args.port || args.p || 8080;
 // Load app middleware here to serve routes, accept data requests, etc.
 //
 // Create and update access log
 // The morgan format below is the Apache Foundation combined format but with ISO8601 dates
-app.use(morgan(':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"',
-    {stream: fs.createWriteStream(path.join(logpath, 'access.log')), flags: 'a' }
-))
+app.use(
+  morgan(
+    ':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"',
+    {
+      stream: fs.createWriteStream(path.join(logpath, "access.log")),
+      flags: "a",
+    }
+  )
+);
 // Serve static files
-const staticpath = args.stat || args.s || process.env.STATICPATH || path.join(__dirname, 'public')
-app.use('/', express.static(staticpath))
+const staticpath =
+  args.stat ||
+  args.s ||
+  process.env.STATICPATH ||
+  path.join(__dirname, "public");
+app.use("/", express.static(staticpath));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //----------------------Endpoints----------------------------
 
-import { rps, rpsls } from './lib/rpsls.js';
+import { rps, rpsls } from "./lib/rpsls.js";
 
 //Check endpoint at /app/ that returns 200 OK
 app.get("/app/", (req, res) => {
-    res.status(200).send("200 OK");
+  res.status(200).send("200 OK");
 });
 
 app.get("/bob/", (req, res) => {
-    res.status(200).send("bob 200 OK");
+  res.status(200).send("bob 200 OK");
 });
 
 //Endpoint /app/rps/ that returns {"player":"(rock|paper|scissors)"}
 app.get("/app/rps/", (req, res) => {
-    res.status(200).send(JSON.stringify(rps()));
+  res.status(200).send(JSON.stringify(rps()));
 });
 
 //Endpoint /app/rpsls/ that returns {"player":"(rock|paper|scissors|lizard|spock)"}
 app.get("/app/rpsls/", (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls()));
+  res.status(200).send(JSON.stringify(rpsls()));
 });
 
 ////Endpoint /app/rps/play/ should accept request bodies in the following form: shot=(rock|paper|scissors) (URLEncoded)
 app.get("/app/rps/play/", (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.query.shot)));
+  res.status(200).send(JSON.stringify(rps(req.query.shot)));
 });
 
 //Endpoint /app/rps/play/ should accept request bodies in the following form: {"shot":"(rock|paper|scissors)"} (JSON)
 app.post("/app/rps/play/", (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.body.shot)));
+  res.status(200).send(JSON.stringify(rps(req.body.shot)));
 });
 
 //Endpoint /app/rpsls/play/ should accept request bodies in the following form: shot=(rock|paper|scissors|lizard|spock) (URLEncoded)
 app.get("/app/rpsls/play/", (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.query.shot)));
+  res.status(200).send(JSON.stringify(rpsls(req.query.shot)));
 });
 
 //Endpoint /app/rpsls/play/ should accept request bodies in the following form: {"shot":"(rock|paper|scissors|lizard|spock)"} (JSON)
 app.post("/app/rpsls/play/", (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.body.shot)));
+  res.status(200).send(JSON.stringify(rpsls(req.body.shot)));
 });
 
 //Endpoint /app/rps/play/(rock|paper|scissors)/
 app.get("/app/rps/play/:shot", (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.params.shot)));
+  res.status(200).send(JSON.stringify(rps(req.params.shot)));
 });
 
 //Endpoint /app/rpsls/play/(rock|paper|scissors|lizard|spock)/
 app.get("/app/rpsls/play/:shot", (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.params.shot)));
+  res.status(200).send(JSON.stringify(rpsls(req.params.shot)));
 });
 
 //Default API endpoint that returns 404 NOT FOUND for any endpoints that are not defined
 app.get("*", (req, res) => {
-    res.status(404).send("404 NOT FOUND");
+  res.status(404).send("404 NOT FOUND");
 });
 
 //------------------------------------------
 
 // Create app listener
 const server = app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
+  console.log(`Server listening on port ${port}`);
 });
 // Create a log entry on start
-let startlog = new Date().toISOString() + ' HTTP server started on port ' + port + '\n'
+let startlog =
+  new Date().toISOString() + " HTTP server started on port " + port + "\n";
 // Debug echo start log entry to STDOUT
 if (args.debug) {
-    console.info(startlog)
-} 
+  console.info(startlog);
+}
 // Log server start to file
-fs.appendFileSync(path.join(logpath, 'server.log'), startlog)
+fs.appendFileSync(path.join(logpath, "server.log"), startlog);
 // Exit gracefully and log
-process.on('SIGINT', () => {
-// Create a log entry on SIGINT
-    let stoppinglog =  new Date().toISOString() + ' SIGINT signal received: stopping HTTP server\n'
-//  Log SIGINT to file
-    fs.appendFileSync(path.join(logpath, 'server.log'), stoppinglog)
-// Debug echo SIGINT log entry to STDOUT
+process.on("SIGINT", () => {
+  // Create a log entry on SIGINT
+  let stoppinglog =
+    new Date().toISOString() +
+    " SIGINT signal received: stopping HTTP server\n";
+  //  Log SIGINT to file
+  fs.appendFileSync(path.join(logpath, "server.log"), stoppinglog);
+  // Debug echo SIGINT log entry to STDOUT
+  if (args.debug) {
+    console.info("\n" + stoppinglog);
+  }
+  // Create a log entry on stop
+  server.close(() => {
+    let stoppedlog = new Date().toISOString() + " HTTP server stopped\n";
+    // Log server stop to file
+    fs.appendFileSync(path.join(logpath, "server.log"), stoppedlog);
+    // Debug echo stop log entry to STDOUT
     if (args.debug) {
-        console.info('\n' + stoppinglog)
+      console.info("\n" + stoppedlog);
     }
-// Create a log entry on stop
-    server.close(() => {
-        let stoppedlog = new Date().toISOString() + ' HTTP server stopped\n'
-// Log server stop to file
-        fs.appendFileSync(path.join(logpath, 'server.log'), stoppedlog)
-// Debug echo stop log entry to STDOUT
-        if (args.debug) {
-            console.info('\n' + stoppedlog)
-        }    
-    })
-})
+  });
+});
